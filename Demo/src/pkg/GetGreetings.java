cedafd6d06aa9b7f617d02ec2da7f41c5c5e9e84
mohit <NewAdmin@Versata>
2013-11-19 16:57:45 +0530

    InIt

---
+++ Demo/src/pkg/GetGreetings.java
@@ -0,0 +1,14 @@
+package pkg;
+
+public class GetGreetings {
+
+	private String greetingMessage;
+
+	public String getGreetingMessage() {
+		return greetingMessage;
+	}
+
+	public void setGreetingMessage(String greetingMessage) {
+		this.greetingMessage = greetingMessage;
+	}
+}
---
+++ Demo/src/pkg/SimpleGreetings.java
@@ -0,0 +1,11 @@
+package pkg;
+
+public class SimpleGreetings {
+
+		public static void main(String[] args) {
+			GetGreetings getGreetings = new GetGreetings();
+			getGreetings.setGreetingMessage("This is a simple greeting msg");
+			System.out.println(getGreetings.getGreetingMessage());
+	}
+
+}
